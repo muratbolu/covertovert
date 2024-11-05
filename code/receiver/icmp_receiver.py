723017db859e804916b925083e94ad783f8c2849
muratbolu <muratbolu2000@gmail.com>
2024-11-05 22:28:06 +0300

    Add comments to code

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,5 @@
 from scapy.all import sniff
 
+#: Using the built-in sniffing tools in ``scapy``, we can set a filter for ICMP packages and sniff
+#: only one, which is going to be a packet from ``icmp_sender.py``
 sniff(filter="icmp", count=1).show()
--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,6 @@
 from scapy.all import send, IP, ICMP
 
+#: ``send()`` function sends packets at Layer 3. (Network layer) Using ``scapy``'s syntax 
+#: for packet encapsulation and the builtin ICMP packet generator, a packet is send to
+#: the host ``receiver`` with TTL of 1 as per assignment specification.
 send(IP(src="sender",dst="receiver",ttl=1)/ICMP())
