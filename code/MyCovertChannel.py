a0a4f35a855d59be2ec02dc8c33721ef6d7f0269
muratbolu <muratbolu2000@gmail.com>
2024-12-29 20:51:20 +0300

    Complex state machines implemented

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -3,54 +3,61 @@ from CovertChannelBase import CovertChannelBase
 from scapy.all import IP, UDP, DNS, sniff
 
 class MyCovertChannel(CovertChannelBase):
-    """
-    - You are not allowed to change the file name and class name.
-    - You can edit the class in any way you want (e.g. adding helper functions); however, there must be a "send" and a "receive" function, the covert channel will be triggered by calling these functions.
-    """
-
-    sender_ip = "172.18.0.2"
-    receiver_ip = "172.18.0.3"
-    port = 12345
+    # TODO: explain this part. I implemented a state machine with two
+    # states, 0 and 1. In config.json, the states start at 0 and if the
+    # sender sends 1, the state changes and starts emitting 1. Similarly,
+    # sending 1 in state 1 changes the state to 0 and start emitting 0.
+    sender_state = None
+    receiver_state = None
     bits = ""
     message = ""
 
     def __init__(self):
-        """
-        - You can edit __init__.
-        """
         pass
 
-    def send(self, log_file_name, parameter1, parameter2):
-        """
-        - In this function, you expected to create a random message (using function/s in CovertChannelBase), and send it to the receiver container. Entire sending operations should be handled in this function.
-        - After the implementation, please rewrite this comment part to explain your code basically.
-        """
+    def determine_sending(self, b, state, change_char):
+        bit = 0 if b == '0' else 1
+        if bit == change_char:
+            state = int(not state)
+            return state
+        else:
+            return state
+
+    def send(self, log_file_name, receiver_ip, port, init_state, change_char):
+        # TODO: write comments and explain
+        if self.sender_state == None:
+            self.sender_state = init_state
         binary_message = self.generate_random_binary_message_with_logging(log_file_name)
         for b in binary_message:
-            # TODO: delete print
-            # print(b)
-            bit = 0 if b == '0' else 1
-            packet = IP(dst=self.receiver_ip) / UDP(dport=self.port) / DNS(tc=bit)
+            bit = self.determine_sending(b, self.sender_state, change_char)
+            packet = IP(dst=receiver_ip) / UDP(dport=port) / DNS(tc=bit)
             CovertChannelBase.send(self, packet)
 
     def stop_condition(self, packet):
         return len(self.message) > 0 and self.message[-1] == '.'
 
-    def packet_handler(self, packet):
-        if UDP in packet and packet[UDP].dport == self.port:
+    def determine_receiving(self, tc, state, change_char):
+        def to_str(b): return "0" if b == 0 else "1"
+        if tc == change_char:
+            # if changing characted is encountered (which is 1 by default)
+            # change state.
+            state = int(not state)
+        # return the state in both cases, the text equivalent
+        return to_str(state)
+
+    def packet_handler(self, packet, port, change_char):
+        if UDP in packet and packet[UDP].dport == port:
             tc = packet[DNS].tc
-            self.bits += "0" if tc == 0 else "1"
+            self.bits += self.determine_receiving(tc, self.receiver_state, change_char)
             if len(self.bits) == 8:
-                # print(f"Bits: {self.bits}")
                 self.message += CovertChannelBase.convert_eight_bits_to_character(self, self.bits)
-                # print(f"Message: {self.message}")
+                # reset the bits every byte
                 self.bits = ""
 
-    def receive(self, parameter1, parameter2, parameter3, log_file_name):
-        """
-        - In this function, you are expected to receive and decode the transferred message. Because there are many types of covert channels, the receiver implementation depends on the chosen covert channel type, and you may not need to use the functions in CovertChannelBase.
-        - After the implementation, please rewrite this comment part to explain your code basically.
-        """
-
-        sniff(iface="eth0", prn=self.packet_handler, stop_filter=self.stop_condition, filter=f"udp port {self.port}")
+    def receive(self, log_file_name, port, init_state, change_char):
+        # TODO: explain more
+        if self.receiver_state == None:
+            self.receiver_state = init_state
+        # TODO: prn is unreadable lambda slop, simplify
+        sniff(iface="eth0", prn=(lambda p: self.packet_handler(p, port, change_char)), stop_filter=self.stop_condition, filter=f"udp port {port}")
         self.log_message(self.message, log_file_name)
